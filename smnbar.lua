646bf15d171ccb54570fd3424a0d5b89cd11a7f7
Dan Dumont <ddumont@gmail.com>
2020-07-01 16:55:44 -0400

    style frame borders

--- smnbar.lua
+++ smnbar.lua
@@ -4,17 +4,23 @@ _addon.version  = '0.1';
 
 require 'common';
 
+local FRAME_PADDING_W = 2;
+local FRAME_PADDING_H = 2;
+local ITEM_SPACING_W = 4;
+local ITEM_SPACING_H = 2;
+
 local BUTTONS = 10;
-local DRAG_W = 10;
+local DRAG_W = 15;
 local BUTTON_W = 64;
 local BUTTON_H = BUTTON_W;
-local BUTTONBAR_W = (BUTTON_W + ImGuiStyleVar_ChildWindowRounding * 2) * BUTTONS;
-local BUTTONBAR_H = BUTTON_H + ImGuiStyleVar_ChildWindowRounding * 2;
-local FRAME_W = DRAG_W + BUTTONBAR_W + ImGuiStyleVar_ChildWindowRounding * 2;
-local FRAME_H = BUTTONBAR_H + ImGuiStyleVar_ChildWindowRounding * 2;
+local BUTTONBAR_W = (BUTTON_W + ITEM_SPACING_W * 2) * BUTTONS; -- 2 layers of padding in the buttons
+local BUTTONBAR_H = BUTTON_H + ITEM_SPACING_H * 2; -- 2 layers of padding in the buttons
+local FRAME_W = DRAG_W + BUTTONBAR_W + FRAME_PADDING_W * 2;
+local FRAME_H = BUTTONBAR_H + FRAME_PADDING_H * 2;
 
 local FRAME_FLAGS = bit.bor(
   ImGuiWindowFlags_NoTitleBar,
+  ImGuiWindowFlags_NoResize,
   ImGuiWindowFlags_NoScrollbar,
   ImGuiWindowFlags_NoScrollWithMouse,
   ImGuiWindowFlags_ShowBorders,
@@ -22,20 +28,20 @@ local FRAME_FLAGS = bit.bor(
 );
 
 ashita.register_event('render', function()
+  imgui.PushStyleVar(ImGuiStyleVar_FramePadding, FRAME_PADDING_W, FRAME_PADDING_H);
+  imgui.PushStyleVar(ImGuiStyleVar_ItemSpacing, ITEM_SPACING_W, ITEM_SPACING_H);
+  imgui.BeginChild('buttonbar', nil, BUTTONBAR_H, true);
   imgui.SetNextWindowSize(FRAME_W, FRAME_H, ImGuiSetCond_Always);
-  -- imgui.SetNextWindowSizeConstraints(BUTTON_H, BUTTON_W, FLT_MAX, FLT_MAX);
   if (not imgui.Begin('smnbar', true, FRAME_FLAGS)) then return imgui.End() end
   imgui.Indent(DRAG_W);
-  imgui.BeginChild('draghandle', nil, BUTTONBAR_H, true);
-  imgui.PushStyleVar(ImGuiStyleVar_ItemSpacing, 1);
   for i = 1, BUTTONS do
     if (i > 1) then imgui.SameLine(); end
     if (imgui.ImageButton(nil, BUTTON_W, BUTTON_H)) then
 
     end
   end
-  imgui.PopStyleVar();
   imgui.EndChild();
   imgui.Unindent(DRAG_W);
   imgui.End();
+  imgui.PopStyleVar(2);
 end);
